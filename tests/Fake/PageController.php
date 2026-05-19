0535249ffc4fab065a411cdc9ef284a31b9e253a
Juan Gadiel Rosado Colón <juangadiel@gmail.com>
2026-05-19 17:27:27 -0500

    Update ArrayList namespace for SilverStripe 6

--- tests/Fake/PageController.php
+++ tests/Fake/PageController.php
@@ -4,7 +4,7 @@ namespace Dynamic\Elements\Blog\Tests\Fake;
 
 use PageController as BasePageController;
 use SilverStripe\Dev\TestOnly;
-use SilverStripe\ORM\ArrayList;
+use SilverStripe\Model\List\ArrayList;
 use SilverStripe\ORM\PaginatedList;
 
 /**
